fef2cbaec84a422b27b46a257865de0a99183466
Kayumov Alexander <killerman323@gmail.com>
2020-07-27 00:34:15 +0300

    Таблица Материал для задач

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task} from '../../model/Task';
 import {DataHandlerService} from '../../service/data-handler.service';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-tasks',
@@ -9,15 +10,34 @@ import {DataHandlerService} from '../../service/data-handler.service';
 })
 export class TasksComponent implements OnInit {
 
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  dataSource: MatTableDataSource<Task>;
   tasks: Task[];
 
-  constructor(private dataHandler: DataHandlerService) { }
+  constructor(private dataHandler: DataHandlerService) {
+  }
 
   ngOnInit(): void {
     this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
+    this.dataSource = new MatTableDataSource();
+    this.refreshTable();
   }
 
   toggleTaskCompleted(task: Task): void {
     task.completed = !task.completed;
+  }
+
+  refreshTable(): void {
+    this.dataSource.data = this.tasks;
+  }
+
+  getPriorityColor(task: Task): string {
+    if (task.completed) {
+      return '#f9f9f9';
+    }
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+    return '#fff';
   }
 }
